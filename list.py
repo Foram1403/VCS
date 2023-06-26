9dee03797f44efae2f9a5d1d2c02f5c8e9887509
Foram1403 <meet042828mp@gmail.com>
2023-06-26 11:46:15 +0530

    First Commit

---
+++ list.py
@@ -0,0 +1,15 @@
+# first method
+a = [1,2,3,4,5.6,'Ni Hao']
+print(a)
+print (len(a))
+print(type(a))
+
+#second method
+b = list((1,2,3,2.5,'Bonjor'))
+print(b)
+print(len(b))
+print(type(b))
+
+
+c = [1,2,3,4,5.6,'Ni Hao']
+print(c[-3])
